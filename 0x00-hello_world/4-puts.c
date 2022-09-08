202f2eea0feed068e041cd4cbe7e08ed9fb30c36
graceliella <graceliella25@gmail.com>
2022-09-08 13:34:53 -0700

    lovely

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-/** 
- * prints exactly "Programming is like building a multilingual puzzle
- * Use the function puts
- */
+
 int main(void)
 {
 	puts("\ "Programming is like building a multilingual puzzle \n " );
