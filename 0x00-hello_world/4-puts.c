9ecc6a48e00240c4d0d0c925e25c2f28a4733e45
graceliella <graceliella25@gmail.com>
2022-09-08 13:32:26 -0700

    hardly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-Int main (void)
-{ 
-	puts(“\ “Programming is like building a multilingual puzzle \n”);
-	Return (0);
+/** 
+ * prints exactly "Programming is like building a multilingual puzzle
+ * Use the function puts
+ */
+int main(void)
+{
+	puts("\ "Programming is like building a multilingual puzzle \n " );
+		return(0);
 }
