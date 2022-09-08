73fc143a2a330257c70e74ac4eac853ac434ba6b
graceliella <graceliella25@gmail.com>
2022-09-08 14:15:13 -0700

    lovely

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - print the sentence "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle \n " );
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle"/n)
+		return (0);
 }
