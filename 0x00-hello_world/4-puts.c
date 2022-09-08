2570663e7ca8e3611daa6e90c60e6a9a3d692987
graceliella <graceliella25@gmail.com>
2022-09-08 14:36:37 -0700

    hardly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the sentence "Programming is like building a multilingual puzzle"
+ * main- Serves as the entry point for the program
+ * Return: By default returns zero if no erros
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"/n)
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
