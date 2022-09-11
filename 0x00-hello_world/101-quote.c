e93ecd31405b73d0a8f375639986651b55b71547
graceliella <graceliella25@gmail.com>
2022-09-11 09:55:25 -0700

    lovel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
 /**
- * Return: 1 on success
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
+	int len;
+
+	len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
