ff5331c89545ead3407136a353d267ff37172be0
graceliella <graceliella25@gmail.com>
2022-09-12 12:11:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,6 @@ int main(void)
 	int len;
 
 	len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 	return (1);
 }
