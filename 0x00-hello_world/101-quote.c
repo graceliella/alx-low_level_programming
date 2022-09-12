470bedac45c8705a209a68da8b99b64afec6f8c0
graceliella <graceliella25@gmail.com>
2022-09-12 12:02:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - main block
  * Return: 1
@@ -8,6 +10,5 @@ int main(void)
 	int len;
 
 	len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
