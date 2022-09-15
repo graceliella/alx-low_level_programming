4580b1fb6798e102d571e874beaaff85093fe6f5
graceliella <graceliella25@gmail.com>
2022-09-15 05:01:27 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,51 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 number
- *
- * Description: fibobnachi numbers starting from one
- * Return: 0
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	long fib1, fib1a, fib1b, fib2, fib2a, fib2b, fib3, fib3a, fib3b;
-	long divi = 10000000000;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	fib1 = 0;
-	fib2 = 1;
-	int counter = 1;
+	j = 1;
+	k = 2;
 
-	for (counter = 0; counter < 91; counter++)
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		fib3 =  fib2 + fib1;
-		fib1 = fib2;
-		fib2 = fib3;
-		printf("%ld, ", fib3);
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	fib1a = fib1 / divi;
-	fib1b = fib1 % divi;
-	fib2a = fib2 / divi;
-	fib2b = fib2 % divi;
-	for (; counter < 98; counter++)
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
 	{
-		fib3a = fib1a + fib2a;
-		fib3b = fib1b + fib2b;
-		if (fib3b > divi)
-		{
-			fib3b = fib3b % divi;
-			fib3a++;
-		}
-		if (counter != 97)
-		{
-			printf("%ld%ld, ", fib3a, fib3b);
-			fib1a = fib2a;
-			fib1b = fib2b;
-			fib2a = fib3a;
-			fib2b = fib3b;
-		}
-		else
-			printf("%ld%ld\n", fib3a, fib3b);
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
+
+	printf("\n");
+
 	return (0);
-}
